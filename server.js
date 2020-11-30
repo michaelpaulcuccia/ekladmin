deccc85ba01c591a2e9075a7b283d717b05651be
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-11-30 11:52:37 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -15,6 +16,15 @@ app.use(cors());
 //routes
 app.use('/admin/users', require('./routes/users'));
 app.use('/admin/orderForms', require('./routes/orderForms'));
+
+//serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
