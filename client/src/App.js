d1f44d10eb95b92fef64cdf2c162bc402b415515
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-01 16:22:10 -0500

    implemented login

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,15 @@ import UsersList from './components/users/UsersList';
 import UserEdit from './components/users/UserEdit';
 import OrderFormList from './components/orderForms/OrderFormList';
 import OrderFormEdit from './components/orderForms/OrderFormEdit';
+import authProvider from './authProvider';
 
 function App() {
   return (
-    <Admin dataProvider={restProvider('https://desolate-stream-40197.herokuapp.com')}>
+    <Admin
+      dataProvider={restProvider('https://desolate-stream-40197.herokuapp.com')}
+      //dataProvider={restProvider('http://localhost:3000')}
+      authProvider={authProvider}
+    >
       <Resource name='admin/users' list={UsersList} edit={UserEdit} />
       <Resource name='admin/orderForms' list={OrderFormList} edit={OrderFormEdit} />
     </Admin>
